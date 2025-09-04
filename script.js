8a6b2ae8b332ad9d7da0e6c632b0fffeb9f3a686
manya-s19 <manya19singla@gmail.com>
2025-09-04 18:06:16 -0400

    new

--- script.js
+++ script.js
@@ -2,9 +2,7 @@
 // Mobile menu toggle
 const toggle = document.querySelector('.menu-toggle');
 const menu = document.querySelector('.menu');
-if(toggle){
-  toggle.addEventListener('click',()=>{
-    if(menu.style.display==='flex'){ menu.style.display='none'; }
-    else{ menu.style.display='flex'; }
-  });
-}
+menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('active');     // show/hide menu
+  menuToggle.classList.toggle('hide'); // hide button when menu is open
+});
